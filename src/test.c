c69482afa5e0babc80ca6836d7219400730ecae9
Robbie Buxton <robbiesbuxton@gmail.com>
2022-07-08 18:58:04 +0100

    feat: compared chain contraction on gemm vs devito

--- src/test.c
+++ src/test.c
@@ -17,54 +17,90 @@
 #include "omp.h"
 
 
+	  // i, j, k, l = dimensions('i j k l')
+    // A = Function(name='A', shape=mat_shape, dimensions=(i, j))
+    // B = Function(name='B', shape=mat_shape, dimensions=(j, k))
+    // C = Function(name='C', shape=mat_shape, dimensions=(j, k))
+    // D = Function(name='D', shape=mat_shape, dimensions=(i, k))
+    // E = Function(name='E', shape=mat_shape, dimensions=(k, l))
+    // F = Function(name='F', shape=mat_shape, dimensions=(i, l))
+    // chain_contractions(A, B, C, D, E, F, optimize)
+
 int main (int argc, char* argv[] ) {
 
 	//Dimentions of the matrix's 
-	// A = i*j, B = j*k, C = i*k-
-	int i = 1000;
-	int j = 1000;
-	int k = 1000;
+	//They are the same for below 320
+
+	int size = 2;
+	int i = size;
+	int j = size;
+	int k = size;
+	int l = size;
 	
-	struct dataobj A_vec, B_vec, gemm_C_vec, devito_C_vec;
+	struct dataobj A_vec, B_vec, C_vec, gemm_D_vec, devito_D_vec, E_vec, gemm_F_vec, devito_F_vec;
 	init_vector(&A_vec,i,j);
 	init_vector(&B_vec,j,k);
+	init_vector(&C_vec,j,k);
+	init_vector(&E_vec,k,l);
+	init_vector(&gemm_D_vec,i,k);
+	init_vector(&devito_D_vec,i,k);
+	init_vector(&gemm_F_vec,i,l);
+	init_vector(&devito_F_vec,i,l);
 
-	fill_matrix(A_vec.data,i,j);
-	fill_matrix(B_vec.data,j,k);
+	//Fills matrix with data (Should update it to be random)
+	fill_matrix(A_vec.data,A_vec.size[0],A_vec.size[1]);
+	fill_matrix(B_vec.data,B_vec.size[0],B_vec.size[1]);
+	fill_matrix(C_vec.data,C_vec.size[0],C_vec.size[1]);
+	fill_matrix(E_vec.data,E_vec.size[0],E_vec.size[1]);
 
-	init_vector(&gemm_C_vec,i,k);
-	init_vector(&devito_C_vec,i,k);
 
 	//Init timers
 	struct profiler gemm_timers = {.section0 = 0};
 	struct profiler devito_timers = {.section0 = 0};
 
 
-	gemm_mult_kernel(&A_vec,&B_vec,&gemm_C_vec,i-1,0,j-1,0,k-1,0,&gemm_timers);
-	devito_mult_kernel(&A_vec,&B_vec,&devito_C_vec,i-1,0,j-1,0,k-1,0,&devito_timers);
+
+	// gemm_mult_kernel(&A_vec,&B_vec,&gemm_C_vec,i-1,0,j-1,0,k-1,0,&gemm_timers);
+	// devito_mult_kernel(&A_vec,&B_vec,&devito_C_vec,i-1,0,j-1,0,k-1,0,&devito_timers);
+	int block_size = 16;
+	int thread_number = 8;
+	devito_chain_contraction_kernel(&A_vec, &B_vec, &C_vec, &devito_D_vec, &E_vec, &devito_F_vec, block_size, i-1,0,j-1,0,k-1,0,l-1,0,thread_number,&devito_timers);
+	gemm_chain_contraction_kernel(&A_vec, &B_vec, &C_vec, &gemm_D_vec, &E_vec, &gemm_F_vec, block_size, i-1,0,j-1,0,k-1,0,l-1,0,thread_number,&gemm_timers);
+
 
 	//PrintArrays
 	if (((i < 10) && (j < 10)) && (k < 10)) {
 		printf("A\n");
-		print_matrix(A_vec.data,i,j);
+		print_matrix(A_vec.data,A_vec.size[0],A_vec.size[1]);
 		printf("B\n");
-		print_matrix(B_vec.data,j,k);
-		printf("GEMM C\n");
-		print_matrix(gemm_C_vec.data,i,k);
-		printf("devito C\n");
-		print_matrix(devito_C_vec.data,i,k);
+		print_matrix(B_vec.data,B_vec.size[0],B_vec.size[1]);
+		printf("C\n");
+		print_matrix(C_vec.data,C_vec.size[0],C_vec.size[1]);
+		printf("GEMM D\n");
+		print_matrix(gemm_D_vec.data,gemm_D_vec.size[0],gemm_D_vec.size[1]);
+		printf("devito D\n");
+		print_matrix(devito_D_vec.data,devito_D_vec.size[0],devito_D_vec.size[1]);
+		printf("E\n");
+		print_matrix(E_vec.data,E_vec.size[0],E_vec.size[1]);
+		printf("GEMM F\n");
+		print_matrix(gemm_F_vec.data,gemm_F_vec.size[0],gemm_F_vec.size[1]);
+		printf("devito F\n");
+		print_matrix(devito_F_vec.data,devito_F_vec.size[0],devito_F_vec.size[1]);
 	}
 	
-	printf("The matrices are the same: %s\n",equal_matrix(A_vec.data,B_vec.data,i,k) ? "true" : "false");
+	printf("The matrices are the same: %s\n",equal_matrix(gemm_F_vec.data,devito_F_vec.data,i,l) ? "true" : "false");
 	printf("GEMM Multiplication took %f seconds\n",gemm_timers.section0);
 	printf("Devito Multiplication took %f seconds\n",devito_timers.section0);
 	
 	//Free array storing matrices 
 	destroy_vector(&A_vec);
 	destroy_vector(&B_vec);
-	destroy_vector(&gemm_C_vec);
-	destroy_vector(&devito_C_vec);
-
+	destroy_vector(&C_vec);
+	destroy_vector(&E_vec);
+	destroy_vector(&gemm_D_vec);
+	destroy_vector(&devito_D_vec);
+	destroy_vector(&gemm_F_vec);
+	destroy_vector(&devito_F_vec);
 
 	return 0;
 }
@@ -95,6 +131,9 @@ int gemm_mult_kernel(struct dataobj *restrict A_vec, struct dataobj *restrict B_
 
 int gemm_chain_contraction_kernel(struct dataobj *restrict A_vec, struct dataobj *restrict B_vec, struct dataobj *restrict C_vec, struct dataobj *restrict D_vec, struct dataobj *restrict E_vec, struct dataobj *restrict F_vec, const int i0_blk0_size, const int i_M, const int i_m, const int j_M, const int j_m, const int k_M, const int k_m, const int l_M, const int l_m, const int nthreads, struct profiler * timers) {
 	START_TIMER(section0)
+
+	//Bounds are very wrong need to fix this
+
 	cblas_sgemm(
 		CblasRowMajor,					//Order - Specifies row-major (C) or column-major (Fortran) data ordering.
 		CblasNoTrans,						//TransA - Specifies whether to transpose matrix A.
@@ -108,7 +147,41 @@ int gemm_chain_contraction_kernel(struct dataobj *restrict A_vec, struct dataobj
 		(float *)B_vec->data,		//Matrix B 
 		j_M + 1, 								//The size of the first dimension of matrix B; if you are passing a matrix B[m][n], the value should be m.
 		0.0, 										//Beta - Scaling factor for matrix C.
-		(float *)C_vec->data, 	//Matrix C
+		(float *)D_vec->data, 	//Matrix C
+		i_M + 1);								//The size of the first dimension of matrix C; if you are passing a matrix C[m][n], the value should be m.
+
+	cblas_sgemm(
+		CblasRowMajor,					//Order - Specifies row-major (C) or column-major (Fortran) data ordering.
+		CblasNoTrans,						//TransA - Specifies whether to transpose matrix A.
+		CblasNoTrans,						//TransB - Specifies whether to transpose matrix B.
+		i_M + 1,								//Number of rows in matrices A and C.
+		k_M + 1,								//Number of columns in matrices B and C.
+		j_M + 1,								//Number of columns in matrix A; number of rows in matrix B.
+		1.0,										//Alpha - Scaling factor for the product of matrices A and B.
+		(float *)A_vec->data, 	//Matrix A.
+		i_M + 1,								//The size of the first dimension of matrix A; if you are passing a matrix A[m][n], the value should be m.
+		(float *)C_vec->data,		//Matrix B 
+		j_M + 1, 								//The size of the first dimension of matrix B; if you are passing a matrix B[m][n], the value should be m.
+		1.0, 										//Beta - Scaling factor for matrix C.
+		(float *)D_vec->data, 	//Matrix C
+		i_M + 1);								//The size of the first dimension of matrix C; if you are passing a matrix C[m][n], the value should be m.
+	STOP_TIMER(section0,timers)
+
+	
+	cblas_sgemm(
+		CblasRowMajor,					//Order - Specifies row-major (C) or column-major (Fortran) data ordering.
+		CblasNoTrans,						//TransA - Specifies whether to transpose matrix A.
+		CblasNoTrans,						//TransB - Specifies whether to transpose matrix B.
+		i_M + 1,								//Number of rows in matrices A and C.
+		k_M + 1,								//Number of columns in matrices B and C.
+		j_M + 1,								//Number of columns in matrix A; number of rows in matrix B.
+		1.0,										//Alpha - Scaling factor for the product of matrices A and B.
+		(float *)D_vec->data, 	//Matrix A.
+		i_M + 1,								//The size of the first dimension of matrix A; if you are passing a matrix A[m][n], the value should be m.
+		(float *)E_vec->data,		//Matrix B 
+		j_M + 1, 								//The size of the first dimension of matrix B; if you are passing a matrix B[m][n], the value should be m.
+		0.0, 										//Beta - Scaling factor for matrix C.
+		(float *)F_vec->data, 	//Matrix C
 		i_M + 1);								//The size of the first dimension of matrix C; if you are passing a matrix C[m][n], the value should be m.
 	STOP_TIMER(section0,timers)
 	return 0;
@@ -140,7 +213,6 @@ int devito_mult_kernel(struct dataobj *restrict A_vec, struct dataobj *restrict 
 
 	return 0;
 }
-
 
 int devito_chain_contraction_kernel(struct dataobj *restrict A_vec, struct dataobj *restrict B_vec, struct dataobj *restrict C_vec, struct dataobj *restrict D_vec, struct dataobj *restrict E_vec, struct dataobj *restrict F_vec, const int i0_blk0_size, const int i_M, const int i_m, const int j_M, const int j_m, const int k_M, const int k_m, const int l_M, const int l_m, const int nthreads, struct profiler * timers)
 {
--- src/utils.c
+++ src/utils.c
@@ -7,7 +7,7 @@ void print_matrix(float *matrix, int n, int m) {
     for(int i =0; i<n; i++){
         printf("|");
         for(int j=0; j<m; j++){
-            printf("%.2f ",matrix[i*n+j]);
+            printf("%f ",matrix[i*n+j]);
         }
         printf("|\n");
     }
@@ -15,9 +15,10 @@ void print_matrix(float *matrix, int n, int m) {
 }
 
 void fill_matrix(float *matrix, int n, int m) {
+		int maxRandVal = 100;
     for(int i =0; i<n; i++){
         for(int j=0; j<m; j++){
-            matrix[n*i + j] = n*i + j;
+            matrix[n*i + j] = (float)rand()/(float)(RAND_MAX/100);
         }
     }
 }
@@ -26,6 +27,8 @@ bool equal_matrix(float *matrix_a,float *matrix_b, int n, int m) {
     for(int i =0; i<n; i++){
         for(int j=0; j<m; j++){
             if (matrix_a[n*i + j] != matrix_b[n*i + j]) {
+								printf("Error matrices are different!!!!!\n");
+								printf("At i: %d, j: %d A = %f, B = %f\n",i,j,matrix_a[n*i + j],matrix_b[n*i + j]);
                 return false; 
             }
         }
